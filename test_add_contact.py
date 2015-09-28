59f7e2f8805f253e05ffb01f6375375dbe478331
yulia-baturina <s-ckrepk-a@ya.ru>
2015-09-28 11:44:44 -0700

    moved from unittest to pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 from dataObjects.contact import Contact
-from commonFunctions import open_home_page
-from commonFunctions import login
-from commonFunctions import logout
-import unittest
+from application import Application
+import pytest
 
 def is_alert_present(wd):
     try:
@@ -13,56 +10,22 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_contact(self):
-        success = True
-        wd = self.wd
-        open_home_page(wd)
-        login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(firstname="first", middlename="", lastname="last", nickname="nick",
-        company="company", title="", address="", homePhone="+11111111111", mobilePhone="", workPhone="",
-        fax="", email="", email2="", email3="", homePage="", birthDay="", birthMonth="", birthYear="",
-        annDay="", annMonth="", annYear="", group="", secondaryAddress="", home="", notes=""))
-        self.return_to_home_page(wd);
-        logout(wd);
 
-    def create_new_contact(self, wd, contact):
-        # init cintact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill in contact fields
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    def cleanup():
+            fixture.destroy()
+    # using request.addfinalizer(fixture.destroy()) causes method destroy()
+    # to be invoked right in the set up, so additional function cleanup() was added to avoid that
+    request.addfinalizer(cleanup)
+    return fixture
 
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
 
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_new_contact(Contact(firstname="first", middlename="", lastname="last", nickname="nick",
+    company="company", title="", address="", homePhone="+11111111111", mobilePhone="", workPhone="",
+    fax="", email="", email2="", email3="", homePage="", birthDay="", birthMonth="", birthYear="",
+    annDay="", annMonth="", annYear="", group="", secondaryAddress="", home="", notes=""))
+    app.logout();
--- test_add_group.py
+++ test_add_group.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+import pytest
 from dataObjects.group import Group
-from commonFunctions import open_home_page
-from commonFunctions import login
-from commonFunctions import logout
-import unittest
+from application import Application
+
 
 def is_alert_present(wd):
     try:
@@ -13,55 +11,24 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_group(self):
-        success = True
-        wd = self.wd
-        open_home_page(wd)
-        login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="new", header="header", footer="footer"))
-        self.return_to_groups_page(wd)
-        logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    def cleanup():
+            fixture.destroy()
+    # using request.addfinalizer(fixture.destroy()) causes method destroy()
+    # to be invoked right in the set up, so additional function cleanup() was added to avoid that
+    request.addfinalizer(cleanup)
+    return fixture
 
-    def test_add_empty_group(self):
-        success = True
-        wd = self.wd
-        open_home_page(wd)
-        login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        logout(wd)
 
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="new", header="header", footer="footer"))
+    app.logout()
 
-    def create_group(self, wd, group):
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group name
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
 
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
